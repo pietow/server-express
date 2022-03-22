e9504c4b43cb9f749160e9f701074e58eddd986b
piet <pprotassow@posteo.de>
2022-03-22 15:28:23 +0100

    feat: get Users

--- modules/user/user.middleware.js
+++ modules/user/user.middleware.js
@@ -5,9 +5,21 @@
 
     module.exports = {
         addUser: addUser,
+        getUsers: getUsers,
     }
 
     const UserService = require('./user.module')().UserService
+
+    function getUsers(req, res, next) {
+        UserService.fetchUsers()
+            .then((data) => {
+                req.response = data
+                next()
+            })
+            .catch((err) => {
+                next(err)
+            })
+    }
 
     function addUser(req, res, next) {
         UserService.createUser(req.body).then(success).catch(failure)
@@ -18,7 +30,7 @@
         }
 
         function failure(err) {
-            next(error)
+            next(err)
         }
     }
 })()
