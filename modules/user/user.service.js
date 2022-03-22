4cbd25e703d56c18f081e98f634323ee336a8065
piet <pprotassow@posteo.de>
2022-03-22 14:14:04 +0100

    feat: fetch all users

--- modules/user/user.service.js
+++ modules/user/user.service.js
@@ -4,9 +4,14 @@
 
     module.exports = {
         createUser: createUser,
+        fetchUsers: fetchUsers,
     }
 
     const UserModel = require('./user.module')().UserModel
+
+    function fetchUsers() {
+        return UserModel.find({}).exec()
+    }
 
     function createUser(user) {
         return UserModel.create(user)
