a9f08611384ad2131adcacd8986c7305af800771
piet <pprotassow@posteo.de>
2022-03-24 14:50:13 +0100

    test: check if password is hashed; fail middleware test

--- tests/integration/user.controller.js
+++ tests/integration/user.controller.js
@@ -15,6 +15,7 @@ const Fixtures = require('../fixtures/fixtures')
 const UserFixture = Fixtures.UserFixture
 
 const baseUri = '/api/users'
+const { comparePassword } = require('../../helpers/password.helper')
 
 describe('UserController', function () {
     describe(`POST ${baseUri}`, function () {
@@ -23,13 +24,21 @@ describe('UserController', function () {
             request(app)
                 .post(baseUri)
                 .send(UserFixture.newUser)
-                .end((err, res) => {
+                .end(async (err, res) => {
+                    const bool = await comparePassword(
+                        'password',
+                        res.body.password,
+                    )
+                    expect(bool).to.be.true
                     expect(res.status).to.equal(201)
                     expect(res.body).to.not.equal({})
                     expect(res.body._id).to.not.equal(undefined)
                     expect(res.body.fname).to.equal(
                         UserFixture.createdUser.fname,
                     )
+                    /* expect(res.body.password).to.equal( */
+                    /*     UserFixture.createdUser.password, */
+                    /* ) */
 
                     done()
                 })
@@ -43,8 +52,8 @@ describe('UserController', function () {
                 .end(function (err, res) {
                     expect(res.status).to.equal(200)
                     expect(res.body).to.not.equal(undefined)
-                    /* expect(res.body).to.be.a('array') */
-                    /* expect(res.body.length).to.not.equal(0) */
+                    expect(res.body).to.be.a('array')
+                    expect(res.body.length).to.not.equal(0)
 
                     done()
                 })
