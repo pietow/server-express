99172a7550287e8fe9d63c48e4b716f3cfa782ba
callnasty <andre@grrlz.net>
2022-04-27 12:23:16 +0200

    fix: email transport bug

--- helpers/mailer.helper.js
+++ helpers/mailer.helper.js
@@ -1,7 +1,6 @@
 /** @format */
 
 const nodemailer = require('nodemailer')
-const nextTick = require('sinon/lib/sinon/util/core/next-tick')
 
 module.exports.sendMail = async (
     fname,
@@ -17,22 +16,28 @@ module.exports.sendMail = async (
         const mailContent = () => {
             switch (mode) {
                 case 'confirmUser':
-                    return `<h1>Email Confirmation for Roam Mate</h1>
+                    return [
+                        `Please confirm your account at roam mate`,
+                        `<h1>Email Confirmation for roam mate</h1>
                         <h2>Hello ${fname} ${lname}</h2>
-                        <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
+                        <p>Thank you for subscribing. Please confirm your email by clicking the following link</p>
                         <a href=http://localhost:${
                             process.env.PORT || 3033
                         }/api/users/${userId}/confirm/> Click here</a>
-                        </div>`
+                        </div>`,
+                    ]
                 case 'resetPassword':
                     // route to /:userId/:password/reset where there's a form that is sending a PUT request to the same route
-                    return `<h1>Reset your password for your Roam Mate account</h1>
+                    return [
+                        `Reset your password at roam mate`,
+                        `<h1>Reset your password for your roam mate account</h1>
                         <h2>Hello ${fname} ${lname}</h2>
                         <p>Have you forgotten your password and do you want to create a new one? Then follow this link:</p>
                         <a href=http://localhost:${
                             process.env.PORT || 3033
                         }/api/users/${userId}/${password}/reset> Click here</a>
-                        </div>`
+                        </div>`,
+                    ]
                 default:
                     break
             }
@@ -47,8 +52,8 @@ module.exports.sendMail = async (
         transport.sendMail({
             from: user,
             to: email,
-            subject: 'Please confirm your account at roam mate',
-            html: mailContent(),
+            subject: mailContent()[0],
+            html: mailContent()[1],
         })
         return transport
     } catch (err) {
